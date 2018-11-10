d6c15e88cb815951a443f1329cd7d4192fa55a9e
jadenswang <jaden.shiteng.wang@gmail.com>
2018-11-10 13:47:27 -0500

    ball

---
+++ src/Game/Ball.java
@@ -0,0 +1,31 @@
+package Game;
+
+import javax.swing.JFrame;
+
+public class Ball
+{
+	private int xLoc;
+	private int yLoc;
+
+	public Ball(int xLoc, int yLoc)
+	{
+		this.xLoc = xLoc;
+		this.yLoc = yLoc;
+	}
+
+	public boolean moveBall(int xChange, int yChange, JFrame frame)
+	{
+		int newXLoc = xLoc + xChange;
+		int newYLoc = yLoc + yChange;
+
+		if (newXLoc < frame.getHeight() && newYLoc < frame.getWidth())
+		{
+			xLoc = newXLoc;
+			yLoc = newYLoc;
+
+			return true;
+		}
+
+		return false;
+	}
+}
