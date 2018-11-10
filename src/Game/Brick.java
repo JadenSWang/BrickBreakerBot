7114f6156b697e16d4461ecf1df85522bf243971
jotkim <josiahkim511@gmail.com>
2018-11-10 16:29:09 -0500

    completion

--- src/Game/Brick.java
+++ src/Game/Brick.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-package Game;
-
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class Brick extends JPanel{
-	
-	private int health;
-	private JLabel healthLabel;
-	
-	public Brick(int h) {
-		
-		setLayout(new FlowLayout());
-		
-		health = h;
-		
-		
-		healthLabel = new JLabel(health +"");
-		healthLabel.setFont(new Font("Helvetica",Font.BOLD,22));
-		healthLabel.setForeground(Color.white);
-		
-		
-		setBackground(Color.BLACK);
-		add(healthLabel);
-		setVisible(true);
-		
-	}
-
-}
-=======
 package Game;
 
 import java.awt.Color;
@@ -74,4 +39,3 @@ public class Brick
 		return yLoc;
 	}
 }
->>>>>>> branch 'ui' of https://github.com/JadenSWang/BrickBreakerBot.git
--- src/Game/BrickBreaker.java
+++ src/Game/BrickBreaker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Game;
 
 import java.awt.Color;
@@ -19,115 +18,9 @@ public class BrickBreaker extends JFrame implements ActionListener {
 	private int curScore;
 
 	private JPanel playArea; // area with all bricks and balls
-	private JPanel[][] panelHolder;	//allows me to change specific elements of the grid
-	
-	
-	public BrickBreaker() {
-
-		setSize(650, 900);
-		setTitle("Brick Breaker");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setBackground(Color.white);
-		setLayout(null);
-
-		record = new JLabel("RECORD : ");
-		score = new JLabel("SCORE    : " + curScore);
-
-		record.setFont(new Font("Helvetica", Font.BOLD, 22));
-		score.setFont(new Font("Helvetica", Font.BOLD, 22));
-
-		record.setBounds(225, 25, 200, 20);
-		score.setBounds(225, 45, 200, 20);
-
-		add(record);
-		add(score);
-
-		playArea = new JPanel();
-		playArea.setBackground(Color.white);
-		playArea.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 0, Color.black));
-		playArea.setBounds(0, 125, 650, 650);
-		
-		playArea.setLayout(new GridLayout(8,6,2,2));
-		
-		panelHolder = new JPanel[8][6];    
-		
-		for(int row = 0; row < panelHolder.length; row++) {
-		   for(int col = 0; col < panelHolder[0].length; col++) {
-			   
-		      panelHolder[row][col] = new JPanel();
-
-		      panelHolder[row][col].setBackground(Color.white);		      
-		      playArea.add(panelHolder[row][col]);
-		
-		   }
-		}
-		
-		add(playArea);
-
-		
-		addRow();
-		setVisible(true);
-
-	}
-	
-	//adds a new row of bricks
-	public void addRow() {
-		
-		int numNewBricks = (int) (Math.random()*4 + 1);
-		
-		ArrayList<Integer> possibleLocs = new ArrayList<Integer>( Arrays.asList(0,1,2,3,4,5));
-		
-		for(int i = 0; i < numNewBricks; i++) {
-			
-			int addLoc = possibleLocs.remove((int)(Math.random() * possibleLocs.size()));
-			
-			panelHolder[0][addLoc].add(new Brick(curScore)) ;
-			
-			
-		}
-		
-		this.repaint();
-		
-		
-	}
-
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public static void main(String[] args) {
-
-		new BrickBreaker();
-	}
-
-}
-=======
-package Game;
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import javax.swing.*;
-
-public class BrickBreaker extends JFrame implements ActionListener
-{
-
-	private JLabel record;
-	private JLabel score;
-	private int curScore;
-
-	private JPanel playArea; // area with all bricks and balls
 	private JPanel[][] panelHolder; // allows me to change specific elements of the grid
 
-	public BrickBreaker()
-	{
+	public BrickBreaker() {
 
 		setSize(650, 900);
 		setTitle("Brick Breaker");
@@ -156,10 +49,8 @@ public class BrickBreaker extends JFrame implements ActionListener
 
 		panelHolder = new JPanel[8][6];
 
-		for (int row = 0; row < panelHolder.length; row++)
-		{
-			for (int col = 0; col < panelHolder[0].length; col++)
-			{
+		for (int row = 0; row < panelHolder.length; row++) {
+			for (int col = 0; col < panelHolder[0].length; col++) {
 				panelHolder[row][col] = new JPanel();
 				playArea.add(panelHolder[row][col]);
 			}
@@ -172,35 +63,33 @@ public class BrickBreaker extends JFrame implements ActionListener
 
 	}
 
-	public void addRow()
-	{
+	// adds a new row of bricks
+	public void addRow() {
 
-		int numNewBricks = (int) (Math.random() * 4 + 2);
+		int numNewBricks = (int) (Math.random() * 4 + 1);
 
 		ArrayList<Integer> possibleLocs = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5));
 
-		for (int i = 0; i < numNewBricks; i++)
-		{
+		for (int i = 0; i < numNewBricks; i++) {
 
 			int addLoc = possibleLocs.remove((int) (Math.random() * possibleLocs.size()));
 
-			panelHolder[0][addLoc] = new Brick(curScore);
+			panelHolder[0][addLoc].add(new Brick(curScore));
 
 		}
 
+		this.repaint();
+
 	}
 
-	public void actionPerformed(ActionEvent arg0)
-	{
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 
 		new BrickBreaker();
 	}
 
 }
->>>>>>> branch 'ui' of https://github.com/JadenSWang/BrickBreakerBot.git
