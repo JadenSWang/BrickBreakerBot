c27e7a1275af162eeea32d99f6880b63d40717a5
Erik Sherman <erikkennethsherman@gmail.com>
2018-11-10 19:26:19 -0500

    moved stuff from BrickBreaker to DrawPanel

--- src/Game/BrickBreaker.java
+++ src/Game/BrickBreaker.java
@@ -21,7 +21,6 @@ public class BrickBreaker extends JFrame implements MouseListener
 	private static final long serialVersionUID = -3946190460435085023L;
 	private JLabel record;
 	private JLabel score;
-	private int curScore;
 
 	private DrawPanel playArea; // area with all bricks and balls
 
@@ -36,8 +35,17 @@ public class BrickBreaker extends JFrame implements MouseListener
 		getContentPane().setBackground(Color.white);
 		setLayout(null);
 
+		playArea = new DrawPanel();
+		playArea.setBackground(Color.white);
+		playArea.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 0, Color.black));
+		playArea.setBounds(0, 125, PLAY_LENGTH, PLAY_LENGTH);
+
+		playArea.setLayout(null);
+
+		add(playArea);
+		
 		record = new JLabel("RECORD : ");
-		score = new JLabel("SCORE    : " + curScore);
+		score = new JLabel("SCORE    : " + playArea.getCurScore());
 
 		record.setFont(new Font("Helvetica", Font.BOLD, 22));
 		score.setFont(new Font("Helvetica", Font.BOLD, 22));
@@ -48,63 +56,16 @@ public class BrickBreaker extends JFrame implements MouseListener
 		add(record);
 		add(score);
 
-		playArea = new DrawPanel();
-		playArea.setBackground(Color.white);
-		playArea.setBorder(BorderFactory.createMatteBorder(5, 0, 5, 0, Color.black));
-		playArea.setBounds(0, 125, PLAY_LENGTH, PLAY_LENGTH);
-
-		playArea.setLayout(null);
-
-		add(playArea);
-
 		setVisible(true);
 
 		while (true)
 		{
 			new Scanner(System.in).nextLine();
-			addRow();
+			playArea.addRow();
 		}
 
 	}
-
-	// adds a new row of bricks
-	public void addRow()
-	{
-
-		int numNewBricks = (int) (Math.random() * 4 + 1);
-
-		ArrayList<Integer> possibleLocs = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5));
-
-		for (int i = 0; i < numNewBricks; i++)
-		{
-
-			int addLoc = possibleLocs.remove((int) (Math.random() * possibleLocs.size()));
-
-			int x = 0;
-
-			// always first row
-			int y = 5;
-
-			if (addLoc != 0)
-				x = (Brick.BRICK_WIDTH * addLoc) + (2 * addLoc);
-
-			playArea.getAllBricks().add(new Brick(curScore, x, y));
-
-		}
-
-		moveDown();
-		repaint();
-	}
-
-	// moves all bricks down one row
-	public void moveDown()
-	{
-
-		for (Brick b : playArea.getAllBricks())
-			b.setYLoc(b.getYLoc() + Brick.BRICK_HEIGHT + 2);
-
-	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent arg0)
 	{
--- src/Game/DrawPanel.java
+++ src/Game/DrawPanel.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JPanel;
 
@@ -17,12 +18,16 @@ public class DrawPanel extends JPanel implements MouseListener
 	private static final long serialVersionUID = 8253100141533033512L;
 	private ArrayList<Brick> allBricks;
 	private ArrayList<Ball> allBalls;
+	private int curScore;
 
 	public DrawPanel()
 	{
 		allBalls = new ArrayList<Ball>();
 		allBricks = new ArrayList<Brick>();
 		this.addMouseListener(this);
+
+		// allBalls.add(new Ball(BrickBreaker.PLAY_LENGTH/2,BrickBreaker.PLAY_LENGTH));
+		allBalls.add(new Ball(BrickBreaker.PLAY_LENGTH / 2, BrickBreaker.PLAY_LENGTH - 21));
 	}
 
 	public void paintComponent(Graphics g)
@@ -42,6 +47,49 @@ public class DrawPanel extends JPanel implements MouseListener
 		}
 	}
 
+	// adds a new row of bricks
+	public void addRow()
+	{
+
+		int numNewBricks = (int) (Math.random() * 4 + 1);
+
+		ArrayList<Integer> possibleLocs = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5));
+
+		for (int i = 0; i < numNewBricks; i++)
+		{
+
+			int addLoc = possibleLocs.remove((int) (Math.random() * possibleLocs.size()));
+
+			int x = 0;
+
+			// always first row
+			int y = 5;
+
+			if (addLoc != 0)
+				x = (Brick.BRICK_WIDTH * addLoc) + (2 * addLoc);
+
+			allBricks.add(new Brick(curScore, x, y));
+
+		}
+
+		moveDown();
+		repaint();
+	}
+	
+	public int getCurScore()
+	{
+		return curScore;
+	}
+
+	// moves all bricks down one row
+	public void moveDown()
+	{
+
+		for (Brick b : allBricks)
+			b.setYLoc(b.getYLoc() + Brick.BRICK_HEIGHT + 2);
+
+	}
+
 	public ArrayList<Brick> getAllBricks()
 	{
 		return this.allBricks;
@@ -55,8 +103,6 @@ public class DrawPanel extends JPanel implements MouseListener
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
